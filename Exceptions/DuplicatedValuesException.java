10c382f047a9cee491b2836dcec8a2407c3e3513
shakedbriller <86188407+shakedbriller@users.noreply.github.com>
2021-09-22 01:36:26 +0300

    Add files via upload

---
+++ Exceptions/DuplicatedValuesException.java
@@ -0,0 +1,14 @@
+package Exceptions;
+
+public class DuplicatedValuesException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DuplicatedValuesException() {
+		super("dupliacted velaue canot be employye and cannot be an visitor the same person");
+	}
+
+
+}
